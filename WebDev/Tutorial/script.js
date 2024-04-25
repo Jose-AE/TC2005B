fc1d7a449771ccfbed91ebad4c6050b1319caab7
Jose-AE <107892696+Jose-AE@users.noreply.github.com>
2024-04-25 13:12:36 -0600

    Done Tutorial site

--- WebDev/Tutorial/script.js
+++ WebDev/Tutorial/script.js
@@ -1,23 +1,51 @@
-const apiUrl = "https://api.thecatapi.com/v1/images/search?limit=3";
+const imageContainer = document.getElementById("images");
+const selector = document.getElementById("selector");
+const button = document.getElementById("mainButton");
 
-const images = [
-  document.getElementById("img1"),
-  document.getElementById("img2"),
-  document.getElementById("img3"),
-];
-
-async function getImgsUrl() {
-  const response = await fetch(apiUrl);
+async function getImgUrl() {
+  const response = await fetch(
+    `https://api.thecatapi.com/v1/images/search?limit=1
+    `
+  );
   const images = await response.json();
   return images;
 }
 
-async function setImageSrc() {
-  const imageUrls = await getImgsUrl(); // Wait for the URL to be fetched
+async function loadImages() {
+  imgCount = parseInt(selector.value);
+  const imageUrls = []; // Wait for the URL to be fetched
 
-  for (let i = 0; i < images.length; i++) {
-    images[i].src = imageUrls[i].url;
+  for (let x = 0; x < imgCount; x++) {
+    const img = await getImgUrl(imgCount);
+    imageUrls.push(img[0]);
   }
+
+  console.log(imageUrls);
+
+  htmlToAdd = "";
+
+  for (let i = 0; i < imageUrls.length; i++) {
+    htmlToAdd += getHtmlBody(imageUrls[i].url);
+  }
+
+  imageContainer.innerHTML = htmlToAdd;
 }
 
-setImageSrc(); // Call the function to set the image src
+button.onclick = loadImages;
+
+function getHtmlBody(imgUrl) {
+  return `<div class="col-lg-4 mb-5">
+  <div class="card h-100 shadow border-0">
+    <img id="img2" class="card-img-top" src="${imgUrl}" alt="..." />
+    <div class="card-body p-4">
+      <div class="badge bg-primary bg-gradient rounded-pill mb-2">
+        Media
+      </div>
+      <a
+        class="text-decoration-none link-dark stretched-link"
+        href="#!"
+      ></a>
+    </div>
+  </div>
+</div>`;
+}
